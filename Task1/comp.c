fce5d028835968474e989ea613a9811d070a308c
Shay Bratslvaky <shay.bratslavsky@gmail.com>
2020-12-03 16:07:27 +0200

    add task1

---
+++ Task1/comp.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <sys/fcntl.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdlib.h>
+
+# define BUFF_SIZE 2
+
+int main(int argc,char** argv) {
+    if (argc!=3){ // incase no 2 files given
+        printf("Not Given 2 parameters\n");
+        exit(1);
+    }
+
+    // assign files to variables
+    char * firstFile = argv[1];
+    char * secondFile = argv[2];
+
+    // init buffers for files
+    char* buf1 = calloc(BUFF_SIZE ,sizeof(char));
+    char* buf2 = calloc(BUFF_SIZE, sizeof(char));
+
+    // open 2 file discriptors
+    int firstFd = open(firstFile, O_RDONLY);
+    int secondFd = open(secondFile, O_RDONLY);
+
+    // Make sure files can be open for reading
+    if (firstFd == -1 || secondFd == -1){
+//        printf("%d,%d",firstFd,secondFd);
+        printf("wasn't able to open files\n");
+        exit(1);
+    }
+
+    // Reading from files first buffer
+    int read1 = read(firstFd, buf1,BUFF_SIZE-1);
+    int read2 = read(secondFd, buf2,BUFF_SIZE-1);
+
+    // while we are able to read we keep reading until end of file
+    while(read1>0 && read2>0){
+//        printf("buf1: %s\n", buf1);
+//        printf("buf2: %s\n", buf2);
+        if (buf1[0] != buf2[0]) // check if buffers read from files are the same
+            exit(1);
+        read1 = read(firstFd,buf1,BUFF_SIZE-1);
+        read2 = read(secondFd,buf2,BUFF_SIZE-1);
+    }
+
+    if (read1!=0 || read2!=0) // if finished 1 file before the other
+        exit(1);
+
+    // clearing allocated buffers and closing files
+    free(buf1);
+    free(buf2);
+    close(firstFd);
+    close(secondFd);
+    printf("FILES ARE THE SAME\n");
+    exit(2);
+    return 0;
+}
---
+++ Task1/ex1.c
@@ -0,0 +1,130 @@
+//
+// Created by Shay_bra on 01/12/2020.
+//
+#include <stdio.h>
+#include <sys/fcntl.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include "string.h"
+
+char** readConfigFile(char* configFile){
+    char * studentsDir = (char*) calloc(1,sizeof(char));
+    char * expectedOutPut = (char*) calloc(1,sizeof(char));
+    char * testInput = (char*) calloc(1,sizeof(char));
+
+    int studentDirSize = 1;
+    int testInputSize = 1;
+    int expectedOutPutSize = 1;
+
+    int EnterCounter = 0;
+    int configFd = open(configFile, O_RDONLY); // open config fille for reading
+
+    if(configFd == -1){
+        printf("wasnt able to open file\n");
+        exit(1);
+    }
+
+    char tmp;
+    int read1 = read(configFd, &tmp,1);
+
+    while(read1>0){
+        if (tmp == '\n'){
+            EnterCounter++;
+        }
+        else{
+            if (EnterCounter==0){
+                studentsDir = (char*) realloc(studentsDir,++studentDirSize);
+                studentsDir[studentDirSize-2] = tmp;
+                expectedOutPut[expectedOutPutSize-1] = '\0';
+            }
+
+            else if (EnterCounter==1){
+                testInput = (char*) realloc(testInput,++testInputSize);
+                testInput[testInputSize-2] = tmp;
+                expectedOutPut[expectedOutPutSize-1] = '\0';
+
+            }
+            else if (EnterCounter==2){
+                expectedOutPut = (char*) realloc(expectedOutPut,++expectedOutPutSize);
+                expectedOutPut[expectedOutPutSize-2] = tmp;
+                expectedOutPut[expectedOutPutSize-1] = '\0';
+            }
+
+        }
+        read1 = read(configFd, &tmp,1);
+    }
+    char** paths = malloc(3*sizeof(char*));
+    paths[0]=studentsDir; paths[1]=testInput; paths[2]=expectedOutPut;
+    close(configFd);
+    return paths;
+}
+
+int calcArraySize(char * arr){
+    char * a = arr;
+    int size=0;
+    while(*a){
+        a++;
+        size++;
+    }
+    return size;
+}
+
+void compileStudentsPrograms(char* studentsPath){
+    struct dirent *de;
+//    int studentsPathSize = calcArraySize(studentsPath);
+    DIR *dr = opendir(studentsPath);
+
+    if (dr == NULL)  // opendir returns NULL if couldn't open directory
+    {
+        printf("Could not open current directory" );
+        exit(1);
+    }
+    while ((de = readdir(dr)) != NULL) {
+        if (strcmp(de->d_name, ".") != 0 && strcmp(de->d_name, "..") != 0) {
+            //TODO move to different function and dont forget to free memmory
+            char * userProgram = (char *) calloc(calcArraySize(studentsPath) + (2*calcArraySize(de->d_name)) + 5, sizeof(char));
+
+            strcpy(userProgram, studentsPath);
+            strcat(userProgram, "/");
+            strcat(userProgram, de->d_name);
+            strcat(userProgram, "/");
+
+            char * programOutPath = calloc(calcArraySize(studentsPath) + calcArraySize(de->d_name) + 11,sizeof(char));
+            strcpy(programOutPath,userProgram);
+
+            strcat(userProgram, de->d_name);
+            strcat(userProgram, ".c");
+            strcat(programOutPath, "main.out");
+
+            execlp("gcc", "gcc", userProgram, "-o", programOutPath, NULL);
+        }
+    }
+        //        printf("%s\n", de->d_name);
+
+    closedir(dr);
+}
+
+
+int main(int argc,char** argv){
+    if (argc!=2){ // incase no 2 files given
+        printf("Not Given Config file\n");
+        exit(1);
+    }
+    char * configFile = argv[1];
+
+    char** paths =  readConfigFile(configFile);
+    char* studentsDir = paths[0];
+    char* expectedOutPut = paths[1];
+    char* testInput = paths[2];
+    compileStudentsPrograms(studentsDir);
+
+    free(studentsDir);
+    free(expectedOutPut);
+    free(testInput);
+    free(paths);
+    return 0;
+}
